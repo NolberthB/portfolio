0b1c7813ce5735fea9808857b5025c184c49de56
NolberthB <betanolberto14@gmail.com>
2023-06-26 22:59:14 -0600

    feat: add footer funtional and responcive

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 
 import { BrowserRouter as Router } from 'react-router-dom'
 import { AppRoutes } from '@/Routes/AppRoutes.jsx'
-import { Navbar } from './Components/Navbar/Navbar'
+import { Navbar } from './Components/Navbar'
+import { Footer } from './Components/Footer'
 
 function App() {
   return (
     <Router>
+      
       <Navbar />
       <AppRoutes /> 
+      <Footer />
 
     </Router>
       
---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import './main.css'
+
+const Footer = () => {
+  return (
+    <footer>
+        <p>&copy; 2023 MADE WHIT LOVE BY NOLBERTH</p>
+    </footer>
+   
+  )
+}
+export { Footer }
